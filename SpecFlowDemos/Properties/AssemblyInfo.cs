42ab2a079695f0aa898c40562e63658e9e876883
Prasanth <getprashanth@yahoo.com>
2020-10-11 07:12:46 +1000

    first copy of api testing code

---
+++ SpecFlowDemos/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SpecFlowDemos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SpecFlowDemos")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4c949e5e-c655-4de5-8d48-c239d463855e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
---
+++ SpecFlowDemos/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Dynamitey.DynamicObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using RestSharp.Serialization.Json;
+using log4net;
+using log4net.Config;
+using FluentAssertions;
+
+namespace SpecFlowDemos
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var client = new RestClient("http://localhost:3000/");           
+            var request = new RestRequest("posts/{post}",Method.GET);
+            request.AddUrlSegment("post", 1);
+            var jsonresponse = client.Execute(request);
+
+            var deserialiserObject = new JsonDeserializer();
+            var dictObj= deserialiserObject.Deserialize<Dictionary<string, string>>(jsonresponse);
+            
+            string response = dictObj["id"].ToString();
+            log.Info("Welcome to log4net logging");
+            log.Info("Value of id is " + response);
+            response.Should().NotBeNull("The value of id should be not null but found :" +response );
+
+            response = dictObj["title"].ToString();
+            log.Info("Value of title is " + response);
+            response.Should().NotBeNull("The value of title should be not null but found :" + response);
+
+
+        }
+    }
+}
